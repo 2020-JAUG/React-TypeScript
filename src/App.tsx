c90052fce4d24a95535314281e7c868e829e3551
Alejo <urbinagonzalezja@gmail.com>
2022-04-15 09:21:38 +0200

    Imported components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Counter } from "./bases/Counter";
+import { CounterBy } from "./bases/CounterBy";
 
 
 function App() {
   return (
     <>
-      <h1>React</h1>
+      <Counter initilValue={ 15 }/>
       <hr />
+
+      <CounterBy/>
     </>
   );
 }
